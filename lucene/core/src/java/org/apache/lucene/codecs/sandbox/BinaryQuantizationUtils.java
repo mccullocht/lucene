7ab5a03be7feba1c1820be4b3089911d455dfa0b
Trevor McCulloch <trevor.mcculloch@mongodb.com>
2024-03-21 11:50:33 -0700

    table decode unquantization

--- lucene/core/src/java/org/apache/lucene/codecs/sandbox/BinaryQuantizationUtils.java
+++ lucene/core/src/java/org/apache/lucene/codecs/sandbox/BinaryQuantizationUtils.java
@@ -4,6 +4,23 @@ import org.apache.lucene.index.VectorSimilarityFunction;
 
 /** Placate tidy. */
 public final class BinaryQuantizationUtils {
+  private static final float[] UQ_FLOAT_VALUES = new float[] {-1.0f, 1.0f};
+  private static final byte[] UQ_BYTE_VALUES = new byte[] {-1, 1};
+
+  // For any byte of input binary vector the index at byte * 8 contains the 8 float values that
+  // would be decoded from it as part of un-quantization. This allows un-quantization to decode 8
+  // values at a time via memcpy instead of one bit at a time.
+  private static final float[] UQ_FLOAT_DECODE_TABLE;
+
+  static {
+    UQ_FLOAT_DECODE_TABLE = new float[256 * 8];
+    for (int b = 0; b < 256; b++) {
+      for (int i = 0; i < 8; i++) {
+        UQ_FLOAT_DECODE_TABLE[b * 8 + i] = UQ_FLOAT_VALUES[(b >> i) & 0x1];
+      }
+    }
+  }
+
   private BinaryQuantizationUtils() {}
 
   public static int byteSize(int dimensions) {
@@ -30,14 +47,21 @@ public final class BinaryQuantizationUtils {
     return binVector;
   }
 
-  private static final float[] UQ_FLOAT_VALUES = new float[] {-1.0f, 1.0f};
+  private static void unQuantizeByte(long b, float[] vector, int offset) {
+    System.arraycopy(UQ_FLOAT_DECODE_TABLE, (int) b * 8, vector, offset, 8);
+  }
 
   public static void unQuantize(long[] binVector, float[] vector) {
     for (int i = 0; i < binVector.length; i++) {
       long d64 = binVector[i];
-      for (int j = 0; j < 64; j++) {
-        vector[i * 64 + j] = UQ_FLOAT_VALUES[(int) (d64 >> j) & 0x1];
-      }
+      unQuantizeByte(d64 & 0xff, vector, i * 64);
+      unQuantizeByte((d64 >> 8) & 0xff, vector, i * 64 + 8);
+      unQuantizeByte((d64 >> 16) & 0xff, vector, i * 64 + 16);
+      unQuantizeByte((d64 >> 24) & 0xff, vector, i * 64 + 24);
+      unQuantizeByte((d64 >> 32) & 0xff, vector, i * 64 + 32);
+      unQuantizeByte((d64 >> 40) & 0xff, vector, i * 64 + 40);
+      unQuantizeByte((d64 >> 48) & 0xff, vector, i * 64 + 48);
+      unQuantizeByte((d64 >> 56) & 0xff, vector, i * 64 + 56);
     }
   }
 
@@ -54,8 +78,6 @@ public final class BinaryQuantizationUtils {
     quantize(vector, binVector);
     return binVector;
   }
-
-  private static final byte[] UQ_BYTE_VALUES = new byte[] {-1, 1};
 
   public static void unQuantize(long[] binVector, byte[] vector) {
     for (int i = 0; i < binVector.length; i++) {
